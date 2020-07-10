56e718f06f81592e7186eb38e4e0ac90260f32a0
Dylan.Ochoa <dryan8a@gmail.com>
2020-07-09 20:34:32 -0700

    Worked on the Tokenizer and refined the grammar

--- Compiler/Tokenizer/Program.cs
+++ Compiler/Tokenizer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
+using System.IO;
 /*
 1. Comment: \/\/.*\n?
 2. Namespace: namespace
@@ -33,16 +35,19 @@ using System.Text.RegularExpressions;
 29. Int literal: (\(-\))?\d+
 30. Increment/Decrement: (\+\+|--)
 31. Logical Operand: (&&|\|\||\^\^|!)
-32. Math Operand: (+|-|*|/|%|&|\||\^|~)
-33. Char literal: '\\?.'
-34. String Literals: \".*?\"
-35. Indentifier: [A-Za-z_]\w* 
+32.	Unary Math Operand: (\(-\)|~)
+33. Binary Math Operand: (+|-|*|/|%|&|\||\^)
+34. Char literal: '\\?.'
+35. String Literals: \".*?\"
+36. Indentifier: [A-Za-z_]\w* 
 */
 namespace Tokenizer
 {
 	public enum TokenTypes
     {
 		Comment,
+		StringLiteral,
+		CharLiteral,
 		Namespace,
 		Function,
 		Class,
@@ -57,7 +62,9 @@ namespace Tokenizer
 		OpenParenthesis,
 		CloseParenthesis,
 		IfStatement,
+		ElseStatement,
 		WhileLoop,
+		ForLoop,
 		WhiteSpace,
 		Comparer,
 		SetVariable,
@@ -73,9 +80,8 @@ namespace Tokenizer
 		IntLiteral,
 		IncrementOrDecrement,
 		LogicalOperand,
-		MathOperand,
-		CharLiteral,
-		StringLiteral,
+		UnaryMathOperand,
+		BinaryMathOperand,
 		Indentifier
     }
 	public class Token
@@ -89,49 +95,65 @@ namespace Tokenizer
         }
     }
 
-    public class Tokenizer
+    public static class Tokenizer
     {
-		Dictionary<Regex, TokenTypes> regexes = new Dictionary<Regex, TokenTypes>();
-		public Tokenizer()
-        {
-			regexes.Add(new Regex("\\/\\/.*\n?"),TokenTypes.Comment);
-			regexes.Add(new Regex("namespace"), TokenTypes.Namespace);
-			regexes.Add(new Regex("function"), TokenTypes.Function);
-			regexes.Add(new Regex("class"), TokenTypes.Class);
-			regexes.Add(new Regex("var"), TokenTypes.VariableInitialization);
-			regexes.Add(new Regex(""), TokenTypes.AccessModifier);
-			regexes.Add(new Regex(""), TokenTypes.EntryPointMarker);
-			regexes.Add(new Regex(""), TokenTypes.Type);
-			regexes.Add(new Regex(""), TokenTypes.OpenRegion);
-			regexes.Add(new Regex(""), TokenTypes.CloseRegion);
-			regexes.Add(new Regex(""), TokenTypes.Semicolon);
-			regexes.Add(new Regex(""), TokenTypes.MemberAccess);
-			regexes.Add(new Regex(""), TokenTypes.OpenParenthesis);
-			regexes.Add(new Regex(""), TokenTypes.CloseParenthesis);
-			regexes.Add(new Regex(""), TokenTypes.IfStatement); 
-			regexes.Add(new Regex(""), TokenTypes.WhileLoop); 
-			regexes.Add(new Regex(""), TokenTypes.WhiteSpace); 
-			regexes.Add(new Regex(""), TokenTypes.Comparer); 
-			regexes.Add(new Regex(""), TokenTypes.SetVariable); 
-			regexes.Add(new Regex(""), TokenTypes.Return);
-			regexes.Add(new Regex(""), TokenTypes.Break);
-			regexes.Add(new Regex(""), TokenTypes.Continue);
-			regexes.Add(new Regex(""), TokenTypes.Null);
-			regexes.Add(new Regex(""), TokenTypes.New);
-			regexes.Add(new Regex(""), TokenTypes.Comma);
-			regexes.Add(new Regex(""), TokenTypes.ArrayOpenBracket);
-			regexes.Add(new Regex(""), TokenTypes.ArrayCloseBracket);
-			regexes.Add(new Regex(""), TokenTypes.BoolLiteral);
-			regexes.Add(new Regex(""), TokenTypes.IntLiteral);
-			regexes.Add(new Regex(""), TokenTypes.IncrementOrDecrement);
-			regexes.Add(new Regex(""), TokenTypes.LogicalOperand);
-			regexes.Add(new Regex(""), TokenTypes.MathOperand);
-			regexes.Add(new Regex(""), TokenTypes.CharLiteral);
-			regexes.Add(new Regex(""), TokenTypes.StringLiteral);
-		}
 		public static List<Token> Tokenize(string Program)
         {
-
+			if (Program.Length == 0) return null;
+			int initialLength = Program.Length;
+			Dictionary<Regex, TokenTypes> regexes = new Dictionary<Regex, TokenTypes>();
+			{
+				regexes.Add(new Regex("\\/\\/.*\n?"), TokenTypes.Comment);
+				regexes.Add(new Regex("\".*?\""), TokenTypes.StringLiteral);
+				regexes.Add(new Regex("'\\?.'"), TokenTypes.CharLiteral);
+				regexes.Add(new Regex("namespace"), TokenTypes.Namespace);
+				regexes.Add(new Regex("function"), TokenTypes.Function);
+				regexes.Add(new Regex("class"), TokenTypes.Class);
+				regexes.Add(new Regex("var"), TokenTypes.VariableInitialization);
+				regexes.Add(new Regex("(\\[public\\]|\\[private\\])"), TokenTypes.AccessModifier);
+				regexes.Add(new Regex("entrypoint"), TokenTypes.EntryPointMarker);
+				regexes.Add(new Regex("(int|bool|string|char|void)"), TokenTypes.Type);
+				regexes.Add(new Regex("\\{"), TokenTypes.OpenRegion);
+				regexes.Add(new Regex("\\}"), TokenTypes.CloseRegion);
+				regexes.Add(new Regex(";"), TokenTypes.Semicolon);
+				regexes.Add(new Regex("\\."), TokenTypes.MemberAccess);
+				regexes.Add(new Regex("\\("), TokenTypes.OpenParenthesis);
+				regexes.Add(new Regex("\\)"), TokenTypes.CloseParenthesis);
+				regexes.Add(new Regex("if"), TokenTypes.IfStatement);
+				regexes.Add(new Regex("else"), TokenTypes.ElseStatement);
+				regexes.Add(new Regex("while"), TokenTypes.WhileLoop);
+				regexes.Add(new Regex("for"), TokenTypes.ForLoop);
+				regexes.Add(new Regex("\\s"), TokenTypes.WhiteSpace);
+				regexes.Add(new Regex("(==|!=|<=|>=|<|>)"), TokenTypes.Comparer);
+				regexes.Add(new Regex("="), TokenTypes.SetVariable);
+				regexes.Add(new Regex("return"), TokenTypes.Return);
+				regexes.Add(new Regex("break"), TokenTypes.Break);
+				regexes.Add(new Regex("continue"), TokenTypes.Continue);
+				regexes.Add(new Regex("null"), TokenTypes.Null);
+				regexes.Add(new Regex("new"), TokenTypes.New);
+				regexes.Add(new Regex(","), TokenTypes.Comma);
+				regexes.Add(new Regex("\\["), TokenTypes.ArrayOpenBracket);
+				regexes.Add(new Regex("\\]"), TokenTypes.ArrayCloseBracket);
+				regexes.Add(new Regex("(true|false)"), TokenTypes.BoolLiteral);
+				regexes.Add(new Regex("(\\(-\\))?\\d+"), TokenTypes.IntLiteral);
+				regexes.Add(new Regex("(\\+\\+|--)"), TokenTypes.IncrementOrDecrement);
+				regexes.Add(new Regex("(&&|\\|\\||\\^\\^|!)"), TokenTypes.LogicalOperand);
+				regexes.Add(new Regex("(\\(-\\)|~)"), TokenTypes.UnaryMathOperand);
+				regexes.Add(new Regex("(\\+|-|\\*|/|%|&|\\||\\^)"), TokenTypes.BinaryMathOperand);
+			}
+			List<Token> tokens = new List<Token>();
+			for (int i = 0; i < initialLength; i++)
+			{
+				foreach (var pair in regexes)
+				{
+					var match = pair.Key.Match(Program.Substring(0,i-(initialLength-Program.Length)+1));
+					if (!match.Success || match.Index != 0) continue;
+					tokens.Add(new Token(pair.Value, match.Value));
+					Program = Program.Substring(i - (initialLength - Program.Length));
+					break;
+				}
+			}
+			return tokens;
         }
 
     }
@@ -139,7 +161,8 @@ namespace Tokenizer
     {
         static void Main(string[] args)
         {
-			 
+			string program = File.ReadAllText(@"Example.dyl");
+			var tokens = Tokenizer.Tokenize(program);
         }
     }
 }
