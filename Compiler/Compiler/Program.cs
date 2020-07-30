ff005a89d0d667d559a7daeed631d59c219895fc
Dylan.Ochoa <dryan8a@gmail.com>
2020-07-29 20:33:42 -0700

    ILDASM Example.dyl

--- Compiler/Compiler/Program.cs
+++ Compiler/Compiler/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using TokenizerNamespace;
 using ParserNamespace;
 using ValidatorNamespace;
+using EmitterNamespace;
 using System.Collections.Generic;
 
 namespace Compiler
@@ -11,12 +12,13 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
-            string program = File.ReadAllText("Example.dyl");
+            string programName = "Example.dyl";
+            string program = File.ReadAllText(programName);
             var tokens = Tokenizer.Tokenize(program.AsSpan());
-            ParseTreeNode head;
-            if(!Parser.CompilationUnitProductionParse(tokens.ToArray().AsSpan(), out head)) throw new Exception("Parser Fail");
-            var symbolsTreeHead = Validator.Validate(head);
-            //Convert to IL here
+            ParseTreeNode compilationUnit;
+            if(!Parser.CompilationUnitProductionParse(tokens.ToArray().AsSpan(), out compilationUnit)) throw new Exception("Parser Fail");
+            var symbolsTreeHead = Validator.Validate(compilationUnit);
+            Emitter.EmitIL(compilationUnit, symbolsTreeHead, programName);
         }
     }
 }
--- Compiler/Emitter/Emitter.cs
+++ Compiler/Emitter/Emitter.cs
@@ -26,22 +26,70 @@ namespace EmitterNamespace
                 var accessModifier = type.Children.FirstOrDefault(a => a.Unit == SyntaxUnit.Token && a.Token.TokenType == TokenTypes.AccessModifier);
                 TypeAttributes typeAttributes = accessModifier != null && accessModifier.Token.Lexeme == "[public]" ? TypeAttributes.Class | TypeAttributes.Public : TypeAttributes.Class | TypeAttributes.NotPublic;
                 var typeBuilder = moduleBuilder.DefineType(typeName, typeAttributes);
+                foreach(var member in type.Children.First(a => a.Unit == SyntaxUnit.Body).Children)
+                {
+                    if(member.Unit == SyntaxUnit.MethodDeclaration)
+                    {
+                        int TypeIndex = 0;
+                        bool isEntryPoint = false;
+                        if(member.Children.Exists(a => a.Unit == SyntaxUnit.Token && a.Token.TokenType == TokenTypes.EntryPointMarker))
+                        {
+                            isEntryPoint = true;
+                            TypeIndex++;
+                        }
+                        accessModifier = member.Children.FirstOrDefault(a => a.Unit == SyntaxUnit.Token && a.Token.TokenType == TokenTypes.AccessModifier);
+                        if(accessModifier != null) TypeIndex++;
+                        MethodAttributes methodAttributes = accessModifier != null && accessModifier.Token.Lexeme == "[public]" ? MethodAttributes.Public : MethodAttributes.Private;
+                        Type methodType = GetTypeFromString(member.Children[TypeIndex].Children[0].Token.Lexeme);
+                        string methodName = member.Children[TypeIndex + 1].Token.Lexeme;
+                        Type[] paramTypes = new Type[member.Children[TypeIndex + 2].Children.Count];
+                        int index = 0;
+                        foreach(var param in member.Children[TypeIndex + 2].Children)
+                        {
+                            paramTypes[index] = GetTypeFromString(param.Children[0].Children[0].Token.Lexeme);
+                            index++;
+                        }
+                        //Add method body to queue for ILGen
+                        var methodBuilder = typeBuilder.DefineMethod(methodName, methodAttributes, methodType, paramTypes);
+                        if(isEntryPoint) assemblyBuilder.SetEntryPoint(methodBuilder);
+                        var ilGen = methodBuilder.GetILGenerator();
+                        ilGen.Emit(OpCodes.Ret);
+                    }
+                    if(member.Unit == SyntaxUnit.VariableDeclaration)
+                    {
+                        int TypeIndex = 0;
+                        accessModifier = member.Children.FirstOrDefault(a => a.Unit == SyntaxUnit.Token && a.Token.TokenType == TokenTypes.AccessModifier);
+                        if (accessModifier != null) TypeIndex++;
+                        FieldAttributes fieldAttributes = accessModifier != null && accessModifier.Token.Lexeme == "[public]" ? FieldAttributes.Public : FieldAttributes.Private;
+                        Type fieldType = GetTypeFromString(member.Children[TypeIndex].Children[0].Token.Lexeme);
+                        string fieldName = member.Children[TypeIndex + 1].Token.Lexeme;
+                        var fieldBuilder = typeBuilder.DefineField(fieldName, fieldType, fieldAttributes);
+                    }
 
-                //loop through the class body and add methods and other members, add all ilGens to a Queue so that we can loop through and generate the IL
-                var fieldBuilder = typeBuilder.DefineField("PrintString", typeof(string), FieldAttributes.Public);
-                var methodBuilder = typeBuilder.DefineMethod("Main", MethodAttributes.Static | MethodAttributes.Public);
-                assemblyBuilder.SetEntryPoint(methodBuilder);
-                var ilGen = methodBuilder.GetILGenerator();
-
-
-
+                }
                 typeBuilder.CreateType();
             }
             
-
-
-            
             assemblyBuilder.Save($"{programName}.exe");
+        }
+
+        public static Type GetTypeFromString(string type)
+        {
+            switch(type)
+            {
+                case "int":
+                    return typeof(int);
+                case "string":
+                    return typeof(string);
+                case "bool":
+                    return typeof(bool);
+                case "char":
+                    return typeof(char);
+                case "void":
+                    return typeof(void);
+                default:
+                    return null;
+            }
         }
     }
 }
